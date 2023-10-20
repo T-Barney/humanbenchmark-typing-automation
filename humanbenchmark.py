04936368eff4093d56750c43ad888bbff6ddf617
Barney Tanczos <116983214+T-Barney@users.noreply.github.com>
2023-10-20 19:04:03 -0400

    Create humanbenchmark.py

---
+++ humanbenchmark.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pyautogui
+
+browser = webdriver.Chrome()
+
+url = "https://humanbenchmark.com/tests/typing"
+browser.get(url)
+
+browser.implicitly_wait(4)
+
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+spans = soup.find_all('span', class_='incomplete')
+text_to_type = ''.join([span.get_text() for span in spans])
+print(text_to_type)
+
+pyautogui.write(text_to_type, interval=0)
+
+time.sleep(10)
